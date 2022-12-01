98e030923b8931bbaad6e5c7b61751170e512677
kd07 <kleindemon07@gmail.com>
2022-12-01 18:19:18 -0500

    updated merge script

--- merge.sh
+++ merge.sh
@@ -1,1 +1,12 @@
+cd ../flipperzero-firmware-wPlugins/
+rm -r -f dist/ build/
+cd assets/
+rm -r -f dolphin/* icons/* slideshow/* resources/dolphin/*/
+rm -f resources/dolphin/manifest.txt
+cd ../applications/settings/dolphin_passport/
+rm -f *
+cd ../../../../kd07CustomGraphics/
 cp -r applications/ assets/ ../flipperzero-firmware-wPlugins/
+cd ../flipperzero-firmware-wPlugins/
+./fbt resources icons dolphin_ext
+./fbt updater_package
