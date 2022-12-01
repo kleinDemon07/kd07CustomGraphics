160bff0f370ee17badff946e1c0a2fdf122d0421
kd07 <kleindemon07@gmail.com>
2022-12-01 18:27:54 -0500

    sync

--- merge.sh
+++ merge.sh
@@ -10,3 +10,5 @@ cp -r applications/ assets/ ../flipperzero-firmware-wPlugins/
 cd ../flipperzero-firmware-wPlugins/
 ./fbt resources icons dolphin_ext
 ./fbt updater_package
+cd ../kd07CustomGraphics/
+cp -r ../flipperzero-firmware-wPlugins/dist/f7-C/f7-update-KD07FAP/ ./
