3cfba55b1811eb587084223b01ac6e502dee4920
kd07 <kleindemon07@gmail.com>
2022-12-10 00:28:32 -0500

    sync

---
+++ buildImgPkg.sh
@@ -0,0 +1,13 @@
+cd firmwareSocket/flipperzero-firmware-wPlugins/
+rm -rf dist/ build/ KD*-*-*.tgz KD*-*-*.zip RM*-*-*.tgz RM*-*-*.zip applications/main/clock/icons/
+cd assets/
+rm -rf dolphin/* icons/* slideshow/* resources/dolphin/*/
+rm -f resources/dolphin/manifest.txt
+cd ../applications/settings/dolphin_passport/
+rm -f *
+cd ../../../../../imagePackage/
+cp -r applications/ assets/ buildRelease.sh fbt_options.py ../firmwareSocket/flipperzero-firmware-wPlugins/
+cd ../firmwareSocket/flipperzero-firmware-wPlugins/
+./fbt resources icons dolphin_ext
+./buildRelease.sh
+cp KD*-*-*.zip KD*-*-*.tgz ../../
