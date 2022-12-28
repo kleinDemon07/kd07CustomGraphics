bea9c6d05f2b3fb229774b7fa176561eb54ec164
Klein Demon <kleindemon07@gmail.com>
2022-12-27 20:44:01 -0500

    Sync buildRelease.sh and fbt_options.py to RM

--- imagePackage/buildRelease.sh
+++ imagePackage/buildRelease.sh
@@ -1,12 +1,12 @@
-rm -rf KD*-*-*.tgz KD*-*-*.zip .sconsign.dblite dist build assets/resources/apps
+rm -rf KD*-*-*.tgz KD*-*-*.zip .sconsign.dblite dist build assets/resources/apps assets/resources/tama_p1
 git pull
 ./fbt updater_package
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
 VER_VAR=`cat scripts/version.py | awk '/VERSION/{ gsub(/[",]/,"",$2); print $2}' | tail -1`
 HASH_VAR=`git rev-parse \`git branch -r --sort=committerdate | tail -1\` | awk '{print substr($0,1,7)}' | tail -1`
-mv dist/f7-C/f7-update-KD07FAP "$VER_VAR-$DATE_VAR-KD$TIME_VAR"
-zip -r "KD$DATE_VAR$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "$VER_VAR-$DATE_VAR-KD$TIME_VAR"
-tar -czvf "KD$DATE_VAR$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "$VER_VAR-$DATE_VAR-KD$TIME_VAR"
+mv dist/f7-C/f7-update-KD07FAP "KD$VER_VAR-$DATE_VAR$TIME_VAR"
+zip -rq "KD$DATE_VAR$TIME_VAR-$VER_VAR-$HASH_VAR.zip" "KD$VER_VAR-$DATE_VAR$TIME_VAR"
+tar -czf "KD$DATE_VAR$TIME_VAR-$VER_VAR-$HASH_VAR.tgz" "KD$VER_VAR-$DATE_VAR$TIME_VAR"
 rm -rf "$VER_VAR-$DATE_VAR-KD$TIME_VAR"
-echo " BUILD COMPLETED, ZIP AND TGZ GENERATED"
+echo " BUILD COMPLETED, ZIP AND TGZ GENERATED FOR KD$DATE_VAR-$TIME_VAR-$VER_VAR-$HASH_VAR"
